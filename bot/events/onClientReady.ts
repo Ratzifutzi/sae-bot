c7e1bb52633442b5a8fa2695860617f5a34aaccd
Ratzifutzi <ratzifutzi@gmail.com>
2024-09-04 20:51:19 +0200

    Initial Commit

--- bot/events/onClientReady.ts
+++ bot/events/onClientReady.ts
@@ -1,11 +1,10 @@
 import { Client, Events } from "discord.js";
-import { db } from "../../bot";
 
 module.exports = {
-    once: true,
-    name: Events.ClientReady,
-    
-    async execute( client: Client ) {
-        console.log(`☑️  Logged in as ${ client.user?.tag }`);
-    }
+	once: true,
+	name: Events.ClientReady,
+
+	async execute(client: Client) {
+		console.log(`☑️  Logged in as ${client.user?.tag}`);
+	}
 }
